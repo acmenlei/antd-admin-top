19cd84136c3f3cc424b40e265964c6b6972bee9c
codeleilei <972761675@qq.com>
2021-09-21 23:24:39 +0800

    fix: 代码修复

--- src/App.vue
+++ src/App.vue
@@ -3,8 +3,8 @@
 </template>
 
 <script lang="ts">
-import BasicLayout from "./layout/basic-layout.vue";
-import { queryUserList } from "./api/user";
+import BasicLayout from "/@/layout/basic-layout.vue";
+import { queryUserList } from "/@/api/user";
 
 export default {
   name: "App",
--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,7 @@
 import { createApp } from 'vue'
 import App from '/@/App.vue'
 import antd from "/@/plugins/ant-design-vue"
-import router from '/@/router/index.js'
+import router from '/@/router'
 import "/@/vue.d"
 
 createApp(App).use(antd).use(router).mount('#app')
