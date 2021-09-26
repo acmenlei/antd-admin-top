9e30ba3191e78addcc182accf814d197e2ca8b61
codeleilei <972761675@qq.com>
2021-09-26 17:38:51 +0800

    fix: 头部布局更新

--- src/layout/header-layout.vue
+++ src/layout/header-layout.vue
@@ -1,6 +1,9 @@
 <template>
   <div class="container-header">
-    <div class="container-header-title">ant-design-vue&nbsp;管理台解决方案</div>
+    <div class="container-header-title">
+      <img src="/public/favicon.ico" alt="" />
+      &nbsp;Vue3后台管理系统解决方案
+    </div>
     <div class="container-header-info">我的</div>
   </div>
 </template>
